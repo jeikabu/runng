33a575700eea0b5482db441a6a0571b81a11ec37
Jake <jeikabu@gmail.com>
2018-09-28 10:28:38 +0800

    Use bindgen to generate bindings for nng

--- build.rs
+++ build.rs
@@ -1,10 +1,14 @@
-// https://docs.rs/cmake/0.1.34/cmake/
+extern crate bindgen;
 extern crate cmake;
 
 use cmake::Config;
-use std::env;
+use std::{
+    env,
+    path::PathBuf,
+};
 
 fn main() {
+    // https://docs.rs/cmake/0.1.34/cmake/
     let dst = Config::new("nng")
         .generator("Ninja")
         .define("CMAKE_BUILD_TYPE", "Release")
@@ -17,4 +21,16 @@ fn main() {
     // That contains output from cmake
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=nng");
+
+    // https://rust-lang-nursery.github.io/rust-bindgen
+    // https://docs.rs/bindgen
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-Inng/src/")
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings");
 }
