97f06a14c2ba79870f1ac03855aafae84ccbfce4
Jake <jeikabu+github@gmail.com>
2018-09-29 14:20:20 +0800

    Try Travis linux code coverage with kcov and remove dummy ubuntu target on appveyor

---
+++ scripts/after_success.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    set TARBALL=v36.tar.gz
+    set KCOV_DIR=kcov-36
+    # https://github.com/codecov/example-rust
+    wget https://github.com/SimonKagstrom/kcov/archive/$TARBALL
+    tar xzf $TARBALL
+    cd $KCOV_DIR
+    mkdir build
+    cd build
+    cmake ..
+    make
+    make install DESTDIR=../../kcov-build
+    cd ../../
+    rm -rf $KCOV_DIR
+    for file in target/debug/runng_sys-[^\.d]; do 
+        mkdir -p "target/cov/$(basename $file)"
+        # Arguments at the end are what would be passed to `cargo test`
+        ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file" -- "tests::"
+    done
+    # Upload reports in current directory
+    # https://github.com/SimonKagstrom/kcov/blob/master/doc/codecov.md
+    bash <(curl -s https://codecov.io/bash)
+fi
