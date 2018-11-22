a93cf3a8772659dbd613876df343067a1536c3e3
Jake <jeikabu+github@gmail.com>
2018-11-22 18:06:36 +0800

    Add missing survey and pair protocols

--- runng-sys/wrapper.h
+++ runng-sys/wrapper.h
@@ -2,12 +2,16 @@
 
 // protocols
 #include "nng/src/protocol/bus0/bus.h"
+#include "nng/src/protocol/pair0/pair.h"
+#include "nng/src/protocol/pair1/pair.h"
 #include "nng/src/protocol/pipeline0/pull.h"
 #include "nng/src/protocol/pipeline0/push.h"
 #include "nng/src/protocol/pubsub0/pub.h"
 #include "nng/src/protocol/pubsub0/sub.h"
 #include "nng/src/protocol/reqrep0/rep.h"
 #include "nng/src/protocol/reqrep0/req.h"
+#include "nng/src/protocol/survey0/respond.h"
+#include "nng/src/protocol/survey0/survey.h"
 
 // transports
 #include "nng/src/transport/inproc/inproc.h"
