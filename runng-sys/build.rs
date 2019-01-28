0f846d91973243dfab1a24a4c517fd874d187db2
jake <jeikabu+github@gmail.com>
2019-01-28 22:24:25 +0800

    Back to always running bindgen

--- runng-sys/build.rs
+++ runng-sys/build.rs
@@ -42,29 +42,16 @@ fn main() {
     // Tell rustc to use nng static library
     println!("cargo:rustc-link-lib=static=nng");
 
-    let target_os_arch = format!(
-        "{}-{}.rs",
-        env::var("CARGO_CFG_TARGET_OS").unwrap(),
-        env::var("CARGO_CFG_TARGET_ARCH").unwrap()
-    );
-    let bindings_subdir = "bindings";
-    let in_path = PathBuf::from(bindings_subdir).join(target_os_arch);
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
-    if cfg!(feature = "regen_bindings") {
-        // https://rust-lang-nursery.github.io/rust-bindgen
-        // https://docs.rs/bindgen
-        let bindings = bindgen::Builder::default()
-            .header("wrapper.h")
-            // This is needed if use `#include <nng.h>` instead of `#include "path/nng.h"`
-            //.clang_arg("-Inng/src/")
-            .generate()
-            .expect("Unable to generate bindings");
-        std::fs::create_dir_all(bindings_subdir.to_owned())
-            .expect("Unable to create binding subdirectory");
-        bindings
-            .write_to_file(in_path.to_owned())
-            .expect(&format!("Couldn't write {:?}", in_path));
-    }
-    std::fs::copy(in_path.to_owned(), out_path.to_owned())
-        .expect(&format!("Unable to copy {:?} to {:?}", in_path, out_path));
+    // https://rust-lang-nursery.github.io/rust-bindgen
+    // https://docs.rs/bindgen
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        // This is needed if use `#include <nng.h>` instead of `#include "path/nng.h"`
+        //.clang_arg("-Inng/src/")
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings");
 }
