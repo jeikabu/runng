4e865b5bd4dd7561bb8b7330d51441a4166cea21
jeikabu <jeikabu@users.noreply.github.com>
2019-03-01 14:02:51 +0800

    Bump rust to 1.33, stop running bindgen and remove clang depdendency on arm32/64
    - Even when using `cfg()` in Cargo.toml to attempt to conditionally
    enable runng-sys feature `build-bindgen`, if it's enabled anywhere it's
    enabled everywhere
    - Tests fails on arm32 because hard-coded size is for 64-bit pointers

--- scripts/qemu_arm64.sh
+++ scripts/qemu_arm64.sh
@@ -2,5 +2,5 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] || [[ "$OSTYPE" == "linux-gnu" ]]; then
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
-    docker run -t -v $(pwd):/usr/src jeikabu/debian-rust:arm64v8-stretch-1.32.0 /bin/bash -c "cd /usr/src; cargo test"
+    docker run -t -v $(pwd):/usr/src jeikabu/debian-rust:arm64v8-stretch-1.33.0 /bin/bash -c "cd /usr/src; cargo test"
 fi
